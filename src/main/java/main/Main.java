f70f708dc322d35db06380fc3b3c14a2fe6a7c48
SNaumov <Sergey.Naumov@lanit-tercom.com>
2018-11-06 19:07:55 +0300

    Added HtmlUnit library

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,5 +9,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Console.mainCircle();
+        //Session ses = new Session("df");
     }
 }
--- src/main/java/webstand/Session.java
+++ src/main/java/webstand/Session.java
@@ -1,6 +1,17 @@
 package webstand;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.List;
+import java.util.logging.Level;
+
 public class Session {
+    WebClient webClient;
+    HtmlPage page;
     boolean loadCase;
     boolean saveCase;
     String caseName;
@@ -9,7 +20,23 @@ public class Session {
     String printparamString;
     String placeholdersString;
 
-    public Session(String caseName) {
+    public Session(String caseName) throws Exception {
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
+
+        webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+
+
+        page = webClient.getPage("http://doctornew-xslt.emias.solit-clouds.ru/web/index.html");
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        List<DomElement> list = page.getElementsByTagName("input");
+        list.size();
 
     }
 }
