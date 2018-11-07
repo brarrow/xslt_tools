7b5d7065b6434a7b8a5f8ffde19628e463c12653
SNaumov <Sergey.Naumov@lanit-tercom.com>
2018-11-07 17:45:27 +0300

    love you

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,6 +9,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         //Console.mainCircle();
-        Session ses = new Session("df");
+        Session ses = new Session("test_200");
     }
 }
--- src/main/java/webstand/Session.java
+++ src/main/java/webstand/Session.java
@@ -1,51 +1,74 @@
 package webstand;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.util.WebConnectionWrapper;
 import org.apache.commons.logging.LogFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import java.util.logging.Level;
+
+
 
 public class Session {
     WebClient webClient;
     HtmlPage page;
+    DomElement nameCaseInp;
     DomElement load;
     DomElement save;
     DomElement getHtml;
     DomElement getPdf;
-    boolean loadCase;
-    boolean saveCase;
     String caseName;
     String xmlString;
     String xsltString;
     String printparamString;
     String placeholdersString;
 
-    public Session(String caseName) throws Exception {
+    private void initWebClient() {
         LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
 
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
         java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
 
+
         webClient = new WebClient(BrowserVersion.CHROME);
         webClient.getOptions().setJavaScriptEnabled(true);
         webClient.getOptions().setThrowExceptionOnScriptError(false);
         webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+    }
+
+    public Session(String caseName) throws Exception {
+        this.caseName = caseName;
+        initWebClient();
+        loadCase();
 
 
-        page = webClient.getPage("http://doctornew-xslt.emias.solit-clouds.ru/web/index.html");
-        webClient.getOptions().setJavaScriptEnabled(true);
-        webClient.getOptions().setThrowExceptionOnScriptError(false);
-        List<DomElement> list = page.getElementsByTagName("input");
-        for (DomElement el : list) {
-            String onclickAttrVal = el.getAttribute("onclick");
-            if (onclickAttrVal.contains("save")) {
-                save = el;
-            }
-        }
+    }
 
+    public void loadCase() throws Exception{
+        URL url = new URL("http://doctornew-xslt.emias.solit-clouds.ru/web/load.api");
+        WebRequest requestSettings = new WebRequest(url, HttpMethod.POST);
+        requestSettings = defaultHeaderForRequest(requestSettings);
+        requestSettings.setRequestBody("{\"name\":\""+this.caseName+"\"}");
+        WebResponse redirectPage = webClient.loadWebResponse(requestSettings);
+        getAllRegionsFromResponse(redirectPage);
+    }
+
+    public void getAllRegionsFromResponse(WebResponse response) {
+        String temp = response.getContentAsString();
+        String[] temps = temp.split("[:\",\":][:\":\":]");
+        temps.clone();
+    }
+
+    public WebRequest defaultHeaderForRequest(WebRequest request) {
+        request.setAdditionalHeader("Accept", "*/*");
+        request.setAdditionalHeader("Content-Type", "application/json");
+        request.setAdditionalHeader("Referer", "http://doctornew-xslt.emias.solit-clouds.ru/web/index.html");
+        request.setAdditionalHeader("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7");
+        request.setAdditionalHeader("Accept-Encoding", "gzip, deflate");
+        return request;
     }
 }
